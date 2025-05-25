bcd2aef8bd1782990c81a50e88964663634b4e93
HAMZA KAMEL <125475648+HamzaKML@users.noreply.github.com>
2025-05-25 12:42:26 +0100

    Update app.py
    Render test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def home():
-  return "TP4 : Bonjour depuis Render ! Votre app Flask est bien en ligne."
+  return "TP4 : Bonjour depuis Render ! Votre app Flask est bien en ligne..... #2nd Commit test#"
 
 if __name__ == '__main__':
   app.run()
